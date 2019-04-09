baf28b1440655e441e396f1d23cd36592c68e5ad
tipson93 <dthipsomphanh@gmail.com>
2019-04-09 19:45:44 +0200

    Revert "kawalek entera"
    This reverts commit cb817cf53fd9072af427d69e4b60395948928f43.
    
    # Conflicts:
    #	app/src/main/java/com/example/androidlab1/MainActivity.java

--- app/src/main/java/com/example/androidlab1/MainActivity.java
+++ app/src/main/java/com/example/androidlab1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.androidlab1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -31,6 +35,19 @@ public class MainActivity extends AppCompatActivity {
         listview.setAdapter(this.adapter);
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this,
+                DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
 
 }
